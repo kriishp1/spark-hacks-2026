a8366340ccabb0d796a8a429b20b205f21c68685
Marcin Gorski <mgorski760@gmail.com>
2026-02-06 22:11:02 -0600

    Updated the layout and formating

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import { supabase } from "./supaBaseClient";
+import { supabase } from "../supaBaseClient";
 import { useNavigate } from "react-router-dom";
-
 
 export default function Login() {
   const [email, setEmail] = useState("");
